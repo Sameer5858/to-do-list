e26faaf0043315d20db5a027785420ccb48742f4
sameer5858 <www.md.sameerkhan5858@gmail.com>
2022-02-12 22:06:22 +0530

    add done except local storage and csss

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import { ToDoList } from "./to-do-list";
-import { Project } from "./create-projects";
-import { Task } from "./create-task";
-import { add } from "date-fns";
+import { addNewTask, modalClose, modalOpen, loadTaskContent } from "./ui";
 
-const projectNavContainer = document.getElementById("project-container");
-const taskContentContainer = document.getElementById("task-container");
-const projectHeader = document.querySelector(".project-header");
 const addTaskBtn = document.getElementById("addTask");
 const addProjectButton = document.getElementById("addProject");
-const modal = document.getElementById("task");
+const newProject = document.getElementById("newProject");
 const overlay = document.getElementById("overlay");
 const modalSubmitAddTask = document.getElementById("taskSubmit");
 const modalCancelTask = document.getElementById("cancelTask");
@@ -18,6 +12,25 @@ const modalDescription = document.getElementById("description");
 const modalDueDate = document.getElementById("dueDate");
 const modalPriority = document.getElementById("priority");
 const fixedProjectButtons = document.querySelectorAll(".fixedBtns");
+addProjectButton.addEventListener("click", () => {
+  newProject.classList.add("active");
+  const projectSubmitButton = document.getElementById("projectSubmit");
+  const cancelProjectButton = document.getElementById("cancelProject");
+  const newProjectInput = document.getElementById("projectTitle");
+  cancelProjectButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    newProject.classList.remove("active");
+    newProjectInput.value = "";
+  });
+  projectSubmitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (newProjectInput.value) {
+      addNewProject(newProjectInput.value);
+      newProject.classList.remove("active");
+      newProjectInput.value = "";
+    }
+  });
+});
 modalCancelTask.addEventListener("click", () => {
   modalClose();
   modalSubmitAddTask.style.display = "block";
@@ -28,6 +41,7 @@ fixedProjectButtons.forEach((btn) => {
     const projectId = e.target.getAttribute("data-project-id");
     addTaskBtn.setAttribute("data-project-id", projectId);
     loadTaskContent(projectId);
+    newProject.classList.remove("active");
   });
 });
 modalSubmitAddTask.addEventListener("click", (e) => {
@@ -53,204 +67,3 @@ overlay.addEventListener("click", () => {
   modalSubmitAddTask.style.display = "block";
   editSubmitTask.style.display = "none";
 });
-function modalOpen() {
-  modal.classList.add("active");
-  overlay.classList.add("active");
-}
-function modalClose() {
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
-  modalTitle.value = "";
-  modalDueDate.value = "";
-  modalDescription.value = "";
-  modalPriority.value = "Low";
-  modalSubmitAddTask.style.display = "block";
-  editSubmitTask.style.display = "none";
-  modalEditOn();
-}
-function modalEditOn() {
-  modalTitle.disabled = false;
-  modalDescription.disabled = false;
-  modalDueDate.disabled = false;
-  modalPriority.disabled = false;
-  modalSubmitAddTask.disabled = false;
-}
-function modalEditOff() {
-  modalTitle.disabled = true;
-  modalDescription.disabled = true;
-  modalDueDate.disabled = true;
-  modalPriority.disabled = true;
-  modalSubmitAddTask.disabled = true;
-}
-const newTodoList = ToDoList;
-
-addNewProject("pefkjeuhfh121ygegyfgegyf");
-addNewProject("pewew232323232fkjeuhfhefugeyf");
-addNewProject("pefkje3232311111uhfhefugefefgygegyfgegyf");
-addNewProject("pefkjeuhfh121ygegyfgegyf");
-let id = newTodoList.projects[4].id;
-
-function deleteTask(projectId, taskId) {
-  const project = newTodoList.getProject(projectId);
-  project.deleteTask(taskId);
-  newTodoList.weekTask();
-  newTodoList.todayTask();
-  loadTaskContent(projectId);
-}
-function deleteProject(projectId) {
-  newTodoList.deleteProject(projectId);
-  projectNavContainer.innerHTML = "";
-  loadProjectsNav();
-}
-function addNewTask(projectId, ...details) {
-  taskContentContainer.innerHTML = "";
-  const project = newTodoList.getProject(projectId);
-  project.addTask(...details);
-  newTodoList.weekTask();
-  newTodoList.todayTask();
-  loadTaskContent(projectId);
-}
-function loadTaskContent(projectId) {
-  const project = newTodoList.getProject(projectId);
-  if (!project) {
-    return;
-  }
-  projectHeader.textContent = `${project.name}`;
-  const tasks = project.tasks;
-  taskContentContainer.innerHTML = "";
-  tasks.forEach((task) => {
-    renderTaskContent(task);
-  });
-}
-function renderTaskContent(task) {
-  const button = document.createElement("button");
-  const titleDiv = document.createElement("div");
-  titleDiv.addEventListener("click", () => {
-    const projectId = titleDiv.getAttribute("data-project-id");
-    const taskId = titleDiv.getAttribute("data-task-id");
-    const project = newTodoList.getProject(projectId);
-    const task = project.getTask(taskId);
-    const edit = document.getElementById("edit");
-    edit.addEventListener("click", () => {
-      modalEditOn();
-      modalSubmitAddTask.style.display = "none";
-      editSubmitTask.style.display = "block";
-      editSubmitTask.addEventListener("click", () => {
-        if (modalTitle.value) {
-          project.deleteTask(taskId);
-          addNewTask(
-            projectId,
-            modalTitle.value,
-            modalDueDate.value,
-            modalDescription.value,
-            modalPriority.value
-          );
-
-          modalClose();
-        }
-      });
-    });
-    modalTitle.value = task.title;
-    modalDescription.value = task.description;
-    modalDueDate.value = task.dueDate;
-    modalPriority.value = task.priority;
-    modalEditOff();
-    modalOpen();
-  });
-
-  const dateDiv = document.createElement("div");
-  const close = document.createElement("img");
-  close.addEventListener("click", (e) => {
-    const projectId = e.target.getAttribute("data-project-id");
-    const taskId = e.target.getAttribute("data-task-id");
-    deleteTask(projectId, taskId);
-  });
-  const checkbox = document.createElement("input");
-  checkbox.addEventListener("click", () => {
-    if (checkbox.checked) {
-      titleDiv.classList.add("done");
-      task.status = true;
-      console.log(task);
-    } else {
-      titleDiv.classList.remove("done");
-      task.status = false;
-      console.log(task);
-    }
-  });
-  close.setAttribute("id", "close");
-  button.setAttribute("data-task-id", `${task.id}`);
-  titleDiv.setAttribute("data-task-id", `${task.id}`);
-  dateDiv.setAttribute("data-task-id", `${task.id}`);
-  close.setAttribute("data-task-id", `${task.id}`);
-  checkbox.setAttribute("data-task-id", `${task.id}`);
-  button.setAttribute("data-project-id", `${task.projectId}`);
-  titleDiv.setAttribute("data-project-id", `${task.projectId}`);
-  dateDiv.setAttribute("data-project-id", `${task.projectId}`);
-  close.setAttribute("data-project-id", `${task.projectId}`);
-  checkbox.setAttribute("date-project-id", `${task.projectId}`);
-  button.classList.add("task");
-  titleDiv.classList.add("task-title");
-  titleDiv.textContent = `${task.title}`;
-  if (task.status) {
-    titleDiv.classList.add("done");
-  }
-  dateDiv.textContent = `${task.dueDate}`;
-  dateDiv.classList.add("duedate");
-  close.src = "./icons/close.svg";
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("id", "taskstatus");
-  button.appendChild(checkbox);
-  button.appendChild(titleDiv);
-  button.appendChild(dateDiv);
-  button.appendChild(close);
-  taskContentContainer.appendChild(button);
-}
-
-function addNewProject(name) {
-  projectNavContainer.innerHTML = "";
-  newTodoList.addProject(name);
-  loadProjectsNav();
-}
-function loadProjectsNav() {
-  newTodoList.projects.forEach((project) => {
-    renderProjectNav(project);
-  });
-}
-function renderProjectNav(project) {
-  if (project.id === "inbox" || project.id === "today" || project.id === "week")
-    return;
-  else {
-    const button = document.createElement("button");
-    const text = document.createElement("div");
-    const img = document.createElement("img");
-    const close = document.createElement("img");
-    close.addEventListener("click", (e) => {
-      const projectId = e.target.getAttribute("data-project-id");
-      deleteProject(projectId);
-      loadTaskContent("inbox");
-      addTaskBtn.setAttribute("data-project-id", "inbox");
-    });
-    button.addEventListener("click", (e) => {
-      const projectId = e.target.getAttribute("data-project-id");
-      if (newTodoList.getProject(projectId)) {
-        addTaskBtn.setAttribute("data-project-id", projectId);
-        loadTaskContent(projectId);
-      }
-    });
-
-    button.classList.add("project");
-    img.src = "./icons/calendar-multiple-check.svg";
-    close.src = "./icons/close.svg";
-    close.setAttribute("data-project-id", `${project.id}`);
-    img.setAttribute("data-project-id", `${project.id}`);
-    text.setAttribute("data-project-id", `${project.id}`);
-
-    button.append(img);
-    text.textContent = project.name;
-    text.classList.add("text");
-    button.appendChild(text);
-    button.appendChild(close);
-    button.setAttribute("data-project-id", `${project.id}`);
-    projectNavContainer.appendChild(button);
-  }
-}
